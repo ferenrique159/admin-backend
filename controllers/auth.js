b3bf582e40771028c9bebbf402e18e7ea3fb3929
Fernando Romero <ferenrique159@gmail.com>
2022-08-29 12:31:01 -0400

    Finalizado autenticación por google identity

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const { response } = require("express");
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -49,6 +50,52 @@ const login = async(req, res = response) => {
 
 };
 
+const googleSignIn = async(req, res = response) => {
+
+    try {
+        const { email, name, picture } = await googleVerify(req.body.token);
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        // Guardar Usuario
+        await usuario.save();
+
+        // generar JWT - Token
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            email,
+            name,
+            picture,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'Token no es valido'
+        });
+    }
+};
+
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
